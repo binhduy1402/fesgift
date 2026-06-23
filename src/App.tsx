91f924783b09522e07339f983ad911aadc991bb2
Bình Duy <166035036+binhduy1402@users.noreply.github.com>
2026-06-23 17:17:19 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -61,9 +61,9 @@ export default function App() {
 
       <Philosophy />
 
-      <Workflow />
-
       <Capabilities />
+
+      <Workflow />
 
       <ContactForm
         prefilledProduct={prefilledProduct}
